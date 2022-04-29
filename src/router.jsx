45b3938eda45fe4cb4ac3814755dfd9085bc889e
spanol <viniciusspanol@gmail.com>
2022-04-29 07:38:29 -0300

    commit
    build Router
    Componente responsável pelas rotas do app.

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Step1 from "./pages/FormSteps/Step1";
-import Step2 from "./pages/FormSteps/Step2";
-import Step3 from "./pages/FormSteps/Step3";
 import Clients from "./pages/Clients";
+import SideBar from "./components/Sidebar";
+import CreateClient from "./pages/CreateClient";
 const Router = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/clients" element={<Clients/>}/>
-        <Route path="/step1" element={<Step1 />} />
-        <Route path="/step2" element={<Step2 />} />
-        <Route path="/step3" element={<Step3 />} />
-    </Routes>
+      <BrowserRouter>
+      <div className="container">
+      <SideBar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/create_client" element={<CreateClient />} />
+          <Route path="/clients" element={<Clients />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
