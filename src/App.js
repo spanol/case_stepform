ab96334fe4489250a53d1d0905a271693bbb2e0f
spanol <viniciusspanol@gmail.com>
2022-05-01 07:00:31 -0300

    commit
    feat React-Toastify
    Adição no código da library React-Toastify que emite notificações extremamente belas.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Router from "./router";
 import ContextProvider from "./context/index";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 // import Form from './components/Form';
 
 function App() {
@@ -8,6 +11,18 @@ function App() {
     <ContextProvider>
       <div className="App">
         <Router />
+        <ToastContainer
+          position="top-right"
+          theme="dark"
+          autoClose={1000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover
+        />
       </div>
     </ContextProvider>
   );
