52a29137fe2c72bc023b6f274ac27e41baf8d9ea
spanol <viniciusspanol@gmail.com>
2022-05-01 17:24:33 -0300

    commit
    feat Succes
    Adição de texto e mais alguns estilos na etapa de conclusão do formulário.

--- src/components/Form/Success.jsx
+++ src/components/Form/Success.jsx
@@ -1,16 +1,20 @@
 import { useStep } from "../../context";
 
 export default function Success() {
-const { setStep } = useStep();
+  const { setStep } = useStep();
 
-    const resetStep = () => {
-        setStep(1);
-    }
+  const resetStep = () => {
+    setStep(1);
+  };
 
   return (
     <>
-    <div>Cadastro concluido com sucesso!!</div>
-    <button onClick={resetStep}>Retornar ao inicio.</button>
+      <div>Cadastro concluido com sucesso!!</div>
+
+      <div className="flex" style={{ flexDirection: 'column'}}>
+      <span>Em breve enviaremos um email com instruções dos passos seguintes</span>
+      <button onClick={resetStep}>Retornar ao inicio.</button>
+      </div>
     </>
-  )
+  );
 }
